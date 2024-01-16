964a2595db9c20f0a2ce381fb3e8b45cb1727361
Ten Bitcomb <tbitcomb@auditboard.com>
2024-01-16 10:45:34 -0800

    Remove comment

--- api/deps.ts
+++ api/deps.ts
@@ -4,7 +4,3 @@ export { default as gptTokenizer } from "gpt-tokenizer/model/gpt-3.5-turbo";
 export { default as html2text } from "html2text";
 export * as playwright from "playwright";
 export * as htmlMarkdown from 'node-html-markdown';
-
-
-
-// import * as chromium from "@playwright/browser-chromium";
